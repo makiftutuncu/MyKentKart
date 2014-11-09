41e9796bc3feaad36c5f82cf2ad0d249bd15cc65
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2014-11-09 17:51:07 +0200

    Added an empty MainActivity

---
+++ app/src/main/java/com/mehmetakiftutuncu/mykentkart/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mehmetakiftutuncu.mykentkart.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mehmetakiftutuncu.mykentkart.R;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/mehmetakiftutuncu/mykentkart/models/KentKart.java
+++ app/src/main/java/com/mehmetakiftutuncu/mykentkart/models/KentKart.java
@@ -19,7 +19,7 @@ public class KentKart {
             Gson gson = new Gson();
             return gson.toJson(this, KentKart.class);
         } catch (Exception e) {
-            Log.error(this, String.format("Failed to convert Kent Kart to json! name: %s, number: %s, nfcId: %s", name, number, nfcId), e);
+            Log.error(this, "Failed to convert Kent Kart to json! name: " + name + ", number: " + number + ", nfcId: " + nfcId, e);
             return null;
         }
     }
@@ -29,7 +29,7 @@ public class KentKart {
             Gson gson = new Gson();
             return gson.fromJson(json, KentKart.class);
         } catch (Exception e) {
-            Log.error(KentKart.class, String.format("Failed to generate Kent Kart from json! json: %s!", json), e);
+            Log.error(KentKart.class, "Failed to generate Kent Kart from json! json: " + json, e);
             return null;
         }
     }
