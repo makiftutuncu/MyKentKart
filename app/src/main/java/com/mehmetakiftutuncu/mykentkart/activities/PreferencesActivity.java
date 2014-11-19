a1bf56794579068f4e6d76d73d74f50e1239013d
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2014-11-19 20:23:27 +0200

    Added preferences for connected transport and it's duration

---
+++ app/src/main/java/com/mehmetakiftutuncu/mykentkart/activities/PreferencesActivity.java
@@ -0,0 +1,78 @@
+package com.mehmetakiftutuncu.mykentkart.activities;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.mehmetakiftutuncu.mykentkart.R;
+import com.mehmetakiftutuncu.mykentkart.utilities.Constants;
+
+public class PreferencesActivity extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initialize();
+        initializeData();
+    }
+
+    private void initialize() {
+        /* Since AppCompat library doesn't have compat version of PreferenceFragment,
+           I had to use PreferenceActivity and therefore these stupid workaround stuff.
+
+           It basically gets the root of an empty layout of a default Activity (not compat stuff),
+           adds a Toolbar manually and sets its content (title, navigation etc.). */
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        View content = root.getChildAt(0);
+        LinearLayout toolbarContainer = (LinearLayout) View.inflate(this, R.layout.activity_preferences, null);
+
+        root.removeAllViews();
+        toolbarContainer.addView(content);
+        root.addView(toolbarContainer);
+
+        Toolbar toolbar = (Toolbar) toolbarContainer.findViewById(R.id.toolbar);
+        toolbar.setTitle(getTitle());
+        toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToKentKartList();
+            }
+        });
+
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    private void initializeData() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        ListPreference listPreference = (ListPreference) findPreference(Constants.PREFERENCE_CONNECTED_TRANSPORT_DURATION);
+        listPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                updateConnectedTransportDurationSummary(preference, (String) newValue);
+                return false;
+            }
+        });
+        String duration = preferences.getString(Constants.PREFERENCE_CONNECTED_TRANSPORT_DURATION, getString(R.string.preferences_connectedTransport_duration_default));
+        updateConnectedTransportDurationSummary(listPreference, duration);
+    }
+
+    private void updateConnectedTransportDurationSummary(Preference preference, String newValue) {
+        preference.setSummary(getString(R.string.preferences_connectedTransport_duration_summary, newValue));
+    }
+
+    private void goToKentKartList() {
+        Intent intent = new Intent(this, KentKartListActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/mehmetakiftutuncu/mykentkart/utilities/Constants.java
+++ app/src/main/java/com/mehmetakiftutuncu/mykentkart/utilities/Constants.java
@@ -8,4 +8,7 @@ public class Constants {
     public static final String KENT_KART_NFC_ID = PACKAGE_NAME + ".kentkart.nfcid";
     public static final String EDIT_MODE        = PACKAGE_NAME + ".editmode";
     public static final String HAS_NFC          = PACKAGE_NAME + ".hasnfc";
+
+    public static final String PREFERENCE_CONNECTED_TRANSPORT_ENABLED = "preference_connectedTransport_enabled";
+    public static final String PREFERENCE_CONNECTED_TRANSPORT_DURATION = "preference_connectedTransport_duration";
 }
